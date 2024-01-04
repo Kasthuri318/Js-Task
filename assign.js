f33a36d3df6779351d8aa65670cd0e650d955da2
Kasthuri318 <132540170+Kasthuri318@users.noreply.github.com>
2024-01-04 14:56:01 +0530

    Add files via upload

---
+++ assign.js
@@ -0,0 +1,9 @@
+//console.log("Village Name:")
+/*console.log("Urivi")*/
+console.log("Husband Name:")
+console.log("Amar")
+console.log("Brothers:")
+console.log("Pavan")
+console.log("Mohan")
+console.log("kittu")
+console.log("Raja")
